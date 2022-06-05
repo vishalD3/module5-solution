50a2886821891ef54f16734d5b63daea02d694ab
vishald3 <koolvishalrulz@gmail.com>
2022-06-05 13:51:17 +0530

    Module 5 code added

--- js/script.js
+++ js/script.js
@@ -82,8 +82,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML,
+  function (responseText){
+  document.querySelector("#main-content").innerHTML=responseText;
+
+  }, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
